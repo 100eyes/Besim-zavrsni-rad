6a49c908fb932f0d645598b3f044562c83042902
100eyes <besim.arnautovic@gmail.com>
2017-08-18 19:44:32 +0200

    added serialport boilerplate

--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+console.log("included rendered,js");
+var serialport = require("serialport");
+var bluetooth = new serialport("/dev/rfcomm0");
+
+bluetooth.write('c',function(err){
+  if(err){
+    return console.log("non-uspjelo, doslo do greske\n Greska je:",err.message);
+  }
+
+  console.log("poslano uspjesno");
+
+});
+
+
+bluetooth.on('error',function(err){
+  console.log("Error: ", err.message);
+});
